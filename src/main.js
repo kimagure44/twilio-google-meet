4fa1486afcaf78353b0b0c118478eebcabd49335
Ivan Lara <kimagure44@yahoo.es>
2022-04-23 00:50:41 +0200

    feat: install new ui framework (vuetify)

--- src/main.js
+++ src/main.js
@@ -2,12 +2,16 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
+import vuetify from './plugins/vuetify';
+import '@babel/polyfill';
 
 Vue.config.productionTip = false;
 
 new Vue({
   router: router,
   store: store,
+  vuetify,
+
   render: (h) => {
     return h(App);
   },
